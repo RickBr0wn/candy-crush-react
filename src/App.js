e04cd743d249c9c88056595709f124a12e76dfad
Rick Brown <ricky.brown.00@gmail.com>
2022-01-02 23:21:35 +0000

    adds game board

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
-function App() {
-	return <div>App!</div>
+import { useEffect, useState } from 'react'
+
+const width = 8
+const candyColors = ['blue', 'green', 'orange', 'purple', 'red', 'yellow']
+
+const App = () => {
+	const [currentColorArrangement, setCurrentColorArrangement] = useState()
+
+	const createBoard = () => {
+		const randomColorArrangement = []
+
+		for (let i = 0; i < width * width; i++) {
+			const randomColor =
+				candyColors[Math.floor(Math.random() * candyColors.length)]
+
+			randomColorArrangement.push(randomColor)
+		}
+		setCurrentColorArrangement(randomColorArrangement)
+	}
+
+	useEffect(() => {
+		createBoard()
+	}, [])
+
+	console.log(currentColorArrangement)
+
+	return (
+		<div className='app'>
+			<div className='game'>
+				{currentColorArrangement &&
+					currentColorArrangement.map((candyColor, index) => (
+						<img
+							key={index}
+							style={{ backgroundColor: candyColor }}
+							alt={candyColor}
+						/>
+					))}
+			</div>
+		</div>
+	)
 }
 
 export default App
